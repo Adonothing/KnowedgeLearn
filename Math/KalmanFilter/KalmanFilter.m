176556c7eee5cc72836b0163c502d4e60e18afe3
Adonothing <134787584+Adonothing@users.noreply.github.com>
2023-10-30 02:49:07 +0800

    2023年10月30日

--- Math/KalmanFilter/KalmanFilter.m
+++ Math/KalmanFilter/KalmanFilter.m
@@ -5,7 +5,8 @@ close all; clear; clc;
 %% 无噪音的理想值
 T = (1 : 100); % 离散的时间序列，单位是s
 p = zeros(1, length(T)); v = zeros(1, length(T)); 
-p(1) = 0;v(1) = 5; % 小车的初始速度是5m/s，未知真实值
+p(1) = 0 % 小车的初始位置
+v(1) = 3 % 小车的初始速度
 a = 0.1; % 加速度
 
 for t = 2 : length(T)
@@ -17,8 +18,9 @@ underline_x = [p; v];
  
 %% 有噪音的实际值，模拟噪音
 % 状态噪音
-r_p = 0.01 * randn(1, length(T));
-r_v = 0.01 * randn(1, length(T));
+sigma_r = 0.01
+r_p = sigma_r * randn(1, length(T));
+r_v = sigma_r * randn(1, length(T));
 x = underline_x;
 for t = 2 : length(T)
     x(1, t) = x(1, t - 1) + x(2, t - 1) + 1/2 * a + r_p(t); % 真实的位置
@@ -26,7 +28,8 @@ for t = 2 : length(T)
 end
 
 % 观测噪音
-Delta_p = 3 * randn(1, length(T)); % GPS测量误差，标准差为3m
+sigma_Delta_p = 3
+Delta_p = sigma_Delta_p * randn(1, length(T)); % GPS测量误差，标准差为3m
 z = x(1, :) + Delta_p; % GPS的观测值，带有测量误差
 
 %% 卡尔曼滤波
@@ -36,12 +39,15 @@ F = [1, 1; 0, 1]; % 状态转移矩阵
 B = [1/2; 1]; % 控制矩阵
 u = a;
 H = [1, 0];
-Q = [0.0001, 0; 0, 0.0001]; % 过程噪声的协方差矩阵，这是一个超参数
-R = 9; % 观测噪声的协方差矩阵，也是一个超参数。因为是一维的，就是一个数
+sigma_Q = 0.01
+Q = [sigma_Q^2, 0; 0, sigma_Q^2]; % 过程噪声的协方差矩阵，这是一个超参数
+sigma_R = 3
+R = sigma_R^2; % 观测噪声的协方差矩阵，也是一个超参数。因为是一维的，就是一个数
+
 
 % 初始化
 hat_x_ = zeros(2, length(T)); hat_x = zeros(2, length(T));
-hat_x(:, 1) = [0; 0]; % 初始状态，[位置, 速度]，就是我们要估计的内容
+hat_x(:, 1) = [0; 0]; % 初始状态，[位置, 速度]，就是我们要估计的内容，开始时都未知，设为0
 P = [0.1, 0; 0, 0.1]; % 先验误差协方差矩阵的初始值，根据经验给出
 
 % 卡尔曼滤波5个关键公式
